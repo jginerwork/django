dfcbc71d2a6fb0676b3fc5c40c1b9e3048f22786
Judit Giner Velázquez <>
2026-02-05 09:42:13 +0100

    tests

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path, include
-from polls import views
-from django.contrib import admin
+from django.urls import path
+from . import views
 
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"), #al anar a aquesta url, django executa la funció index
-    path("<int:question_id>/", views.DetailView.as_view(), name="detail"),
-    path("<int:question_id>/results/", views.ResultsView.as_view(), name = "results"),
+    path("", views.IndexView.as_view(), name="index"),
+    
+    # CAMBIO AQUÍ: de <int:question_id> a <int:pk>
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    
+    # CAMBIO AQUÍ: de <int:question_id> a <int:pk>
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    
+    # ESTE SE DEJA IGUAL: porque la función 'vote' en views.py recibe 'question_id'
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
